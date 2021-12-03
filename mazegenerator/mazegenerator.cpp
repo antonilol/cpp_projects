4d46fd0643ae9abbd2ceeb6c13684d4ee9913307
Antoni Spaanderman <56turtle56@gmail.com>
2021-12-03 15:10:22 +0100

    mazegenerator style=2,3

--- mazegenerator/mazegenerator.cpp
+++ mazegenerator/mazegenerator.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <climits>
 
+#define FULL_BLOCK "██"
+
 #define MUST_BE " must be an odd number larger than 2\n"
 #define INVAL_ARG "Invalid argument\n"
 #define USAGE1 "Usage: "
@@ -71,8 +73,16 @@ void print_maze() {
 		std::cout << '+' << std::string(w * 2, '-') << '+';
 	} else if (style == 1) {
 		for (uint i = 0; i < w + 2; i++) {
-			std::cout << "██";
+			std::cout << FULL_BLOCK;
 		}
+	} else if (style < 4) {
+		std::cout << get_char(0b01100 | style == 3);
+		for (uint x = 0; x < w; x++) {
+			for (uint i = 0; i < !(x & 1) + 1; i++) { // TODO optimize
+				std::cout << get_char(0b01010 | (maze[x]) << 2 | style == 3);
+			}
+		}
+		std::cout << get_char(0b00110 | style == 3);
 	}
 
 	for (uint y = 0; y < h; y++) {
@@ -80,40 +90,54 @@ void print_maze() {
 		if (style == 0) {
 			std::cout << "|";
 		} else if (style == 1) {
-			std::cout << "██";
+			std::cout << FULL_BLOCK;
 		} else if (style < 4) {
+			std::cout << get_char(0b10100 | (maze[y * w]) << 3 | (style == 3));
 		}
 		for (uint x = 0; x < w; x++) {
 			if (maze[y * w + x]) {
 				if (style < 2) {
-					std::cout << (style == 0 ? "##" : "██");
+					std::cout << (style == 0 ? "##" : FULL_BLOCK);
 				} else if (style < 4) {
-					std::cout << get_char(
-						(y     == 0 || maze[y * w + x - w]) << 4 |
-						(x + 1 == w || maze[y * w + x + 1]) << 3 |
-						(y + 1 == h || maze[y * w + x + w]) << 2 |
-						(x     == 0 || maze[y * w + x - 1]) << 1 |
-						 style == 3
-					);
+					for (uint i = 0; i < !(x & 1) + 1; i++) { // TODO optimize
+						std::cout << get_char(
+							(y     == 0 || maze[y * w + x - w]) << 4 |
+							(x + 1 == w || maze[y * w + x + 1]) << 3 |
+							(y + 1 == h || maze[y * w + x + w]) << 2 |
+							(x     == 0 || maze[y * w + x - 1]) << 1 |
+							 style == 3
+						);
+					}
 				}
 			} else {
-				std::cout << (style < 2 ? "  " : " ");
+				std::cout << (style < 2 || !(x & 1) ? "  " : " ");
 			}
 		}
 		if (style == 0) {
 			std::cout << '|';
 		} else if (style == 1) {
-			std::cout << "██";
+			std::cout << FULL_BLOCK;
+		} else if (style < 4) {
+			std::cout << get_char(0b10100 | (maze[(y + 1) * w - 1]) << 1 | (style == 3));
 		}
 	}
 
+	std::cout << "\n";
 	if (style == 0) {
-		std::cout << "\n+" << std::string(w * 2, '-') << "+\n";
+		std::cout << std::string(w * 2, '-') << "+\n";
 	} else if (style == 1) {
-		std::cout << "\n████";
+		std::cout << FULL_BLOCK FULL_BLOCK;
 		for (uint i = 0; i < w; i++) {
-			std::cout << "██";
+			std::cout << FULL_BLOCK;
 		}
+	} else if (style < 4) {
+		std::cout << get_char(0b11000 | style == 3);
+		for (uint x = 0; x < w; x++) {
+			for (uint i = 0; i < !(x & 1) + 1; i++) { // TODO optimize
+				std::cout << get_char(0b01010 | (maze[(h - 1) * w + x]) << 4 | style == 3);
+			}
+		}
+		std::cout << get_char(0b10010 | style == 3);
 	}
 }
 
